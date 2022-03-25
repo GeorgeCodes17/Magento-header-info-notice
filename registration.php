4bd4753f5c2b5ab5eb997074fc2b13a2f18a9c79
George Osborn <george@verve-design.co.uk>
2022-03-25 13:28:55 +0000

    Info Notice Initial Commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php 
+// This is where the module name is declared
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Training_InfoNotice',
+    __DIR__
+);
